7cb7fb38680d2048c75e0835c24c0fb7c40130db
Yassine-Ben-Ghanem <benghanem1999@gmail.com>
2022-05-19 09:16:02 +0100

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  CustomNotificationExample
+//
+
+import Foundation
---
+++ ios/CustomNotification.swift
@@ -0,0 +1,8 @@
+@objc(CustomNotification)
+class CustomNotification: NSObject {
+
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+}
